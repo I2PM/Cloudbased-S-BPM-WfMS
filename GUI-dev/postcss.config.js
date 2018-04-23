d4d091d785e86b8e640b3e66f9c8d38b4e289557
Amar Bajric <amar.bajric78@gmail.com>
2018-04-23 15:16:27 +0200

    Fix GUI-dev dependencies (#64)
    * First changes and adaptations for fixing gui-dev:
    - fixed first errors with webpack and dependency errors
    
    * updated/removed/modified package.json and its modules. Added postcss.config.js to solve one issue with css loading of ng-admin library (idk what this is)

---
+++ GUI-dev/postcss.config.js
@@ -0,0 +1,10 @@
+/**
+ * Author: Amar Bajric
+ * https://github.com/akveo/ngx-admin/issues/604#issuecomment-271974780
+ * I have no idea what I am doing...
+ */
+module.exports = {
+  plugins: [
+    require('autoprefixer')({ browsers: ['last 3 versions', '> 1%'] })
+  ]
+};
