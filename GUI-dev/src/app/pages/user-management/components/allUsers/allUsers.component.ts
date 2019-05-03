158b4fcc78eb1d46838d34eeb0c94fd1d9821876
Conny <lahnsteiner51@gmail.com>
2019-05-03 23:20:14 +0200

    adapt lint syntax

--- GUI-dev/src/app/pages/user-management/components/allUsers/allUsers.component.ts
+++ GUI-dev/src/app/pages/user-management/components/allUsers/allUsers.component.ts
@@ -3,7 +3,6 @@ import {ProcessesService} from '../../../../allProcesses.service';
 import {Router, ActivatedRoute} from '@angular/router';
 import {GatewayProvider} from '../../../../@theme/providers/backend-server/gateway';
 import {User} from '../../../../../models/models';
-import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
 import {Toast, ToasterConfig, ToasterService} from 'angular2-toaster';
 
 
@@ -24,7 +23,7 @@ export class AllUsersComponent implements OnInit {
   users: User[];
 
   constructor(protected service: ProcessesService, protected route: ActivatedRoute, protected router: Router,
-              private gateway: GatewayProvider, private modalService: NgbModal,
+              private gateway: GatewayProvider,
               private toasterService: ToasterService) {
 
     this.config = new ToasterConfig({
