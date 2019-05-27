6e03154341c7fe57150213680e6834ac7467004f
Alexander Fink <alexander.fink2@edu.fh-joanneum.at>
2019-05-27 11:53:00 +0200

    [62] deleted obsolete dashboard components

--- GUI-dev/src/app/pages/dashboard/dashboard.module.ts
+++ GUI-dev/src/app/pages/dashboard/dashboard.module.ts
@@ -2,11 +2,6 @@ import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
 
 
 import { ThemeModule } from '../../@theme/theme.module';
-import {BusinessObjectsComponent} from './components/activeProcessDetail/businessObjects.component';
-import {StartableProcessesComponent} from './components/startableProcesses/startableProcesses.component';
-import {TerminatedProcessesComponent} from './components/terminatedProcesses/terminatedProcesses.component';
-import {ActiveProcessDetailComponent} from './components/activeProcessDetail/activeProcessDetail.component';
-import { ActiveProcessesComponent} from './components/activeProcesses/activeProcesses.component';
 import {FormsModule} from '@angular/forms';
 import {DashboardComponent} from './dashboard.component';
 import {ReviewProcessesComponent} from './components/reviewProcesses/reviewProcesses.component';
@@ -27,12 +22,7 @@ import {MyProcessesComponent} from "./components/myProcesses";
   ],
   declarations: [
     DashboardComponent,
-    ActiveProcessesComponent,
-    ActiveProcessDetailComponent,
     MyProcessesComponent,
-    TerminatedProcessesComponent,
-    StartableProcessesComponent,
-    BusinessObjectsComponent,
     ReviewProcessesComponent,
     ValidatedProcessesComponent,
     ModalComponent,
