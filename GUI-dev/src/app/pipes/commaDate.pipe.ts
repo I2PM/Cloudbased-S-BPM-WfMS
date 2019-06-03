08c2e8ae1f1cd99f5e898724908eb220b2e81608
andikay <xtrmwar@gmx.at>
2019-06-03 19:32:13 +0200

    TSLint madness, pt.3

--- GUI-dev/src/app/pipes/commaDate.pipe.ts
+++ GUI-dev/src/app/pipes/commaDate.pipe.ts
@@ -4,6 +4,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class CommaDatePipe implements PipeTransform {
   transform(values: string, args: string[]): Date {
     const month = parseInt(values[1]) || 0;
-    return new Date(parseInt(values[0]) || 0, month > 0 ? month -1 : month, parseInt(values[2]) || 0, parseInt(values[3]) || 0, parseInt(values[4]) || 0, parseInt(values[5]) || 0);
+    return new Date(parseInt(values[0]) || 0, month > 0 ? month - 1 : month, parseInt(values[2]) || 0, parseInt(values[3]) || 0, parseInt(values[4]) || 0, parseInt(values[5]) || 0);
   }
 }
--- GUI-dev/src/app/pipes/engineDate.pipe.ts
+++ GUI-dev/src/app/pipes/engineDate.pipe.ts
@@ -1,4 +1,4 @@
-import {Pipe, PipeTransform} from "@angular/core";
+import {Pipe, PipeTransform} from '@angular/core';
 
 @Pipe({name: 'engineDate'})
 export class EngineDatePipe implements PipeTransform {
@@ -6,13 +6,12 @@ export class EngineDatePipe implements PipeTransform {
     const regex = /^(\d*),(\d*),(\d*),(\d*),(\d*),(\d*)/;
 
     // In some cases, no seconds are delivered
-    if (regex.exec(values)[6])
-    {
-      return regex.exec(values)[4] + ":" + regex.exec(values)[5] +  " "
-        + regex.exec(values)[3] + "." + regex.exec(values)[2] + "." + regex.exec(values)[1];
+    if (regex.exec(values)[6]) {
+      return regex.exec(values)[4] + ':' + regex.exec(values)[5] +  ' '
+        + regex.exec(values)[3] + '.' + regex.exec(values)[2] + '.' + regex.exec(values)[1];
     }
     // Former string was year, month, day, hour, minute, second
-    return regex.exec(values)[4] + ":" + regex.exec(values)[5] + ":" + regex.exec(values)[6] + " "
-      + regex.exec(values)[3] + "." + regex.exec(values)[2] + "." + regex.exec(values)[1];
+    return regex.exec(values)[4] + ':' + regex.exec(values)[5] + ':' + regex.exec(values)[6] + ' '
+      + regex.exec(values)[3] + '.' + regex.exec(values)[2] + '.' + regex.exec(values)[1];
   }
 }
